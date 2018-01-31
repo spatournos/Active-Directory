f8f98aac6ac73243e6e683df6bce39cd4c3959d1
spatournos <32541428+spatournos@users.noreply.github.com>
2018-01-31 20:58:25 +0200

    Update getADGroupMembers.ps1

--- getADGroupMembers.ps1
+++ getADGroupMembers.ps1
@@ -22,7 +22,10 @@
 .EXAMPLE
   \getADGroupMembers.ps1
  #>
-foreach ($pvg in Get-Content .\privategroups.txt){
-Get-ADGroupMember $pvg | Get-ADUser -Property displayName | 
-select @{name="AM";Expression={$_.samaccountname}},@{name="USER";Expression={$_.displayname}} | 
-Export-Csv -path .\"$pvg".csv -encoding "utf8" -NoTypeInformation}
+ 
+foreach ($pvg in Get-Content .\privategroups.txt)
+{
+  Get-ADGroupMember $pvg | Get-ADUser -Property displayName | 
+  select @{name="AM";Expression={$_.samaccountname}},@{name="USER";Expression={$_.displayname}} | 
+  Export-Csv -path .\"$pvg".csv -encoding "utf8" -NoTypeInformation
+}
